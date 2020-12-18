e3dcb2fb040de83c6a3482544ce7156eaf755593
daminduLiyanage <damindu7@gmail.com>
2020-12-19 00:46:10 +0530

    checking code with db code ok

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Button, TextInput} from 'react-native';
+import database from '@react-native-firebase/database';
+import BookScreen from './BookScreen';
+
+const reference = database().ref('/codes/').once('value');
+
+reference.then((snapshot) => {
+  console.log('Codes from DB: ', snapshot.val());
+});
 
 function HomeScreen({navigation}) {
+  const [code, setCode] = useState('');
+  const handleCode = () => {
+    reference.then((snapshot) => {
+      snapshot.val().map((key) => {
+        console.log('each code', key);
+        console.log('comparing ', key, typeof key, ' to code ', typeof code);
+        if (code === key) {
+          console.log('key correct!');
+          navigation.navigate('Book');
+        }
+      });
+    });
+  };
   return (
     <View>
-      <TextInput placeholder=" - code - " />
-      <Button title="login" onPress={() => navigation.navigate('Book')} />
+      <TextInput
+        placeholder=" - code - "
+        onChangeText={(val) => setCode(val)}
+        defaultValue={code}
+      />
+      <Button title="validate" onPress={() => handleCode()} />
     </View>
   );
 }
