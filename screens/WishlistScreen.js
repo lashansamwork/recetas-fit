862aa783f35ff49ea537a510af5922ca2e90b7c8
daminduLiyanage <damindu7@gmail.com>
2020-12-25 23:28:11 +0530

    test vector btn added and it works with warnings

--- screens/WishlistScreen.js
+++ screens/WishlistScreen.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import ActionButton from 'react-native-action-button';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 import WishlistItem from '../components/WishlistItem';
 
 const WishlistScreen = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.view1}>
-        <Text style={styles.text1}> WishlistScreen </Text>
-      </View>
-      <View style={styles.view2}>
-        <WishlistItem title="testItem" quantity="2kg" />
-      </View>
+      <ActionButton buttonColor="rgba(231,76,60,1)">
+        <View>
+          <ActionButton.Item
+            buttonColor="#9b59b6"
+            title="New Task"
+            onPress={() => console.log('notes tapped!')}>
+            <Icon name="md-create" style={styles.actionButtonIcon} />
+          </ActionButton.Item>
+        </View>
+        <View style={styles.view1}>
+          <Text style={styles.text1}> WishlistScreen </Text>
+        </View>
+        <View style={styles.view2}>
+          <WishlistItem title="testItem" quantity="2kg" />
+        </View>
+      </ActionButton>
     </View>
   );
 };
