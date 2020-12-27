43e68e8d789448dd02d4c0663a41e60d4890ff77
Lashan Sam <lashansam@Lashans-Mac-mini.local>
2020-12-26 18:08:53 -0700

    clean up header color

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import * as React from 'react';
-import {AppRegistry} from 'react-native';
+import {AppRegistry, StatusBar} from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { name as appName } from './app.json';
 import Colors from './theme/colors'
@@ -22,6 +22,7 @@ const theme = {
 export default function Main() {
   return (
     <PaperProvider theme={theme}>
+      <StatusBar barStyle="light-content"/>
       <App />
     </PaperProvider>
   );
--- navigation/WelcomeNavigator.js
+++ navigation/WelcomeNavigator.js
@@ -3,14 +3,30 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../screens/ValidationScreen';
 import WelcomeScreen from '../screens/WelcomeScreen';
+import Colors from '../theme/colors'
 
 const Stack = createStackNavigator();
+
+const getHeaderOptions = (title) => {
+  const options = {
+    title: title,
+    headerStyle: {
+      backgroundColor: Colors.themeLightColors.primary,
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
+
+  return options;
+}
 
 function WelcomeNavigator() {
   return (
       <Stack.Navigator initialRouteName="Recetas Fit">
-        <Stack.Screen name="Recetas Fit" component={WelcomeScreen} />
-        <Stack.Screen name="Validation" component={HomeScreen} />
+        <Stack.Screen name="Recetas Fit" component={WelcomeScreen} options={()=>getHeaderOptions()}/>
+        <Stack.Screen name="Validation" component={HomeScreen} options={()=>getHeaderOptions()}/>
       </Stack.Navigator>
   );
 }
