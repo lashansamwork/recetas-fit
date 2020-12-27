9854398b532dc793dec65502d015f9c64a9c8672
daminduLiyanage <damindu7@gmail.com>
2020-12-27 16:59:17 +0530

    section one added

--- navigation/DrawerContent.js
+++ navigation/DrawerContent.js
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native"
-import React, { useContext} from 'react';
+import { View, StyleSheet } from "react-native"
+import React, { useContext } from 'react';
 import {
     DrawerItemList,
     DrawerItem,
@@ -12,61 +12,220 @@ import { PageContext } from "../context-store/PageContextProvider"
 
 function DrawerContent(props) {
 
-    const {state} = props
-    const {routes, index} = state; //Not sure about the name of index property. Do check it out by logging the 'state' variable.
+    const { state } = props
+    const { routes, index } = state; //Not sure about the name of index property. Do check it out by logging the 'state' variable.
     const focusedRoute = routes[index];
     const { changePage } = React.useContext(PageContext);
 
     return (
         <DrawerContentScrollView {...props}>
-            <DrawerItem 
+            <DrawerItem
                 label="Book Home"
-                focused={focusedRoute.name ==="Book Screen"}
-                onPress={()=>props.navigation.navigate('Book Screen')}
+                focused={focusedRoute.name === "Book Screen"}
+                onPress={() => {
+                    props.navigation.navigate('Book Screen')
+                }
+
+                }
                 activeTintColor={Colors.themeLightColors.primary}
             />
             <View style={{ paddingLeft: layout.padding.medium }}>
                 <Drawer.Item
-                    label="Information"
+                    label="Indice"
                     active={false}
-                    onPress={() =>{
-                        changePage(5);
+                    onPress={() => {
+                        changePage(2);
                         props.navigation.closeDrawer();
                     }}
                 />
                 <Drawer.Item
-                    label="Section 1"
+                    label="Antes de empezar"
                     active={false}
-                    onPress={() =>{
+                    style={styles.topic}
+                    onPress={() => {
                         changePage(10);
                         props.navigation.closeDrawer();
                     }}
                 />
+
                 <Drawer.Item
-                    label="Section 2"
+                    label="Sobre nosotros"
                     active={false}
-                    onPress={() =>{
+                    onPress={() => {
+                        changePage(11);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                <Drawer.Item
+                    label="Guia de iconos"
+                    active={false}
+                    onPress={() => {
+                        changePage(12);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                <Drawer.Item
+                    label="Consejos Importantes"
+                    active={false}
+                    onPress={() => {
+                        changePage(13);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                <Drawer.Item
+                    label="Perder grasa"
+                    active={false}
+                    onPress={() => {
                         changePage(15);
                         props.navigation.closeDrawer();
                     }}
                 />
                 <Drawer.Item
-                    label="Section 3"
+                    label="Recordatorios"
                     active={false}
-                    onPress={() =>{
+                    onPress={() => {
+                        changePage(17);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                <Drawer.Item
+                    label="Un libro para todos. Msj para"
+                    active={false}
+                    onPress={() => {
+                        changePage(18);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                <Drawer.Item
+                    label="Recomendados del super"
+                    active={false}
+                    onPress={() => {
+                        changePage(19);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Esenciales en tu cocina"
+                    active={false}
+                    onPress={() => {
                         changePage(20);
                         props.navigation.closeDrawer();
                     }}
                 />
+
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Desayunos"
+                    active={false}
+                    onPress={() => {
+                        changePage(34);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Comidas"
+                    active={false}
+                    onPress={() => {
+                        changePage(49);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Cenas"
+                    active={false}
+                    onPress={() => {
+                        changePage(64);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Guarniciones"
+                    active={false}
+                    onPress={() => {
+                        changePage(79);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Snacks"
+                    active={false}
+                    onPress={() => {
+                        changePage(87);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Postres"
+                    active={false}
+                    onPress={() => {
+                        changePage(112);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Salsas saladas"
+                    active={false}
+                    onPress={() => {
+                        changePage(139);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Salsas dulces"
+                    active={false}
+                    onPress={() => {
+                        changePage(149);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+                <Drawer.Item
+                    style={styles.topic}
+                    label="Zona tropical"
+                    active={false}
+                    onPress={() => {
+                        changePage(155);
+                        props.navigation.closeDrawer();
+                    }}
+                />
+                {/* end */}
+
+
+
+
             </View>
-            <DrawerItem 
+            <DrawerItem
                 label="My WishList"
-                focused={focusedRoute.name ==="My Wishlist"}
-                onPress={()=>props.navigation.navigate('My Wishlist')}
+                focused={focusedRoute.name === "My Wishlist"}
+                onPress={() => props.navigation.navigate('My Wishlist')}
                 activeTintColor={Colors.themeLightColors.primary}
             />
         </DrawerContentScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    topic: {
+        backgroundColor: '#f0f0f7'
+    }
+})
 
 export default DrawerContent;
