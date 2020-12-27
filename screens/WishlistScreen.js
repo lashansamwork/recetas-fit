6bd113330d9b625b178e123f9d715df62fa4be7f
Lashan Sam <lashansam@Lashans-Mac-mini.local>
2020-12-27 02:18:41 -0700

    wishlist code completed

--- screens/WishlistScreen.js
+++ screens/WishlistScreen.js
@@ -1,123 +1,135 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  Modal,
-  TouchableHighlight,
-  TextInput,
+  Text
 } from 'react-native';
 import ActionButton from 'react-native-action-button';
-import {FlatList} from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { Button } from 'react-native-paper';
-
+import { FlatList } from 'react-native-gesture-handler';
+import Colors from "../theme/colors"
+import { Button, Title, TextInput, ActivityIndicator, Subheading } from 'react-native-paper';
 import WishlistItem from '../components/WishlistItem';
-
-const data = [
-  {
-    title: 'apple',
-    quantity: '2kg',
-  },
-  {
-    title: 'orange',
-    quantity: '1kg',
-  },
-  {
-    title: 'pineapple',
-    quantity: '5kg',
-  },
-];
+import layout from '../theme/layout';
+import Modal from 'react-native-modal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Toast from 'react-native-simple-toast';
 
 const WishlistScreen = () => {
   const [modalVisible, setModalVisible] = useState(false);
-  const [wishlistArr, setwishlistArr] = useState(data);
-  const [textInput2Value, setTextInput2Value] = useState(null);
-  const [textInput3Value, setTextInput3Value] = useState(null);
+  const [loading, setLoading]= useState(true);
+  const [wishlistArr, setWishlistArr] = useState([]);
+  const [itemName, setItemName] = useState(null);
+  const [itemQuantity, setItemQuantity] = useState(null);
 
-  const renderItem = ({item}) => {
-    return <WishlistItem title={item.title} quantity={item.quantity} />;
+  useEffect(()=>{
+    getWishList()
+  }, []);
+
+  useEffect(()=>{
+    if(wishlistArr){
+      settWishList()
+    }
+  }, [wishlistArr]);
+
+
+  const settWishList = async ()=> {
+    const jsonValue = JSON.stringify(wishlistArr);
+    AsyncStorage.setItem('@wishList', jsonValue);
+  }
+
+
+  const getWishList = async ()=> {
+    const wishListJson = await AsyncStorage.getItem('@wishList');
+    const wishListData = wishListJson != null ?  JSON.parse(wishListJson) : null;
+    if(wishListData){
+      setWishlistArr(wishListData);
+    }
+    setTimeout(()=>{
+      setLoading(false);
+    }, 1000)
+  }
+
+
+  const renderItem = ({ item }) => {
+    return <WishlistItem title={item.title} quantity={item.quantity} id={item.id} onDeletePressed={onDeletePressed} />;
   };
 
+  const onDeletePressed = (id) => {
+    const updatedWishList = wishlistArr.filter((item) => item.id !== id);
+    setWishlistArr(updatedWishList);
+  }
+
+  if(loading){
+    return (<View style={{ flex: 1, padding: layout.padding.xxxLarge}}>
+      <ActivityIndicator animating={true} color={Colors.accent} />
+    </View>)
+  }
+
   return (
-    <View style={styles.container}>
-      <Modal animationType="slide" transparent={true} visible={modalVisible}>
-        <View style={styles.view3}>
-          <View style={styles.view4}>
-            <View style={styles.view9}>
-              <View style={styles.view5}>
-                <Text>Add your grocery item</Text>
-              </View>
-              <View style={styles.view6}>
-                <TextInput
-                  style={styles.textInput2}
-                  onChangeText={(value) => {
-                    setTextInput2Value(value);
-                  }}
-                />
-              </View>
-              <View style={styles.view7}>
-                <TextInput
-                  keyboardType="decimal-pad"
-                  style={styles.textInput3}
-                  onChangeText={(value) => {
-                    setTextInput3Value(value);
-                  }}
-                />
-              </View>
-              <View style={styles.view8}>
-                {/* <TouchableHighlight
-                  title="save"
-                  style={styles.touchable1}
-                  onPress={() => console.log('ok')}
-                  onPress={() => {
-                    setModalVisible();
+    <View style={{ flex: 1, padding: layout.padding.xxxLarge }}>
+      <Modal isVisible={modalVisible} onBackdropPress={()=>setModalVisible(false)} avoidKeyboard={true}>
+        <View style={{ backgroundColor: 'white', borderRadius: layout.radius.image, padding: layout.padding.large}}>
+          <Title>Add your grocery item</Title>
+          <View style={{ paddingTop: layout.padding.medium }}/>
+          <TextInput
+            mode="outlined"
+            label="Item name"
+            onChangeText={(val) => setItemName(val)}
+          />
+          <View style={{ paddingTop: layout.padding.medium }}/>
+          <TextInput
+            mode="outlined"
+            label="Quantitiy"
+            onChangeText={(val) => setItemQuantity(val)}
+          />
+          <View style={{ flexDirection: 'row', paddingTop: layout.padding.large }}>
+            <View style={{ flex: 1 }}>
+              <Button mode="outlined" onPress={() => setModalVisible(false)}>
+                Cancel
+              </Button>
+            </View>
+            <View style={{ flex: 0.2 }}/>
+            <View style={{ flex: 1 }}>
+              <Button mode="contained" onPress={() => {
+                const wishListArrayLength = wishlistArr.length;
 
-                    setwishlistArr(
-                      wishlistArr.push({textInput2Value, textInput3Value}),
-                    );
-                  }
-                > */}
+                if(!!!itemName){
+                  Toast.show('Item name cannot be empty');
+                  return;
+                } else if(!!!itemQuantity){
+                  Toast.show('Item quantity cannot be empty');
+                  return;
+                }
 
-                <TouchableHighlight
-                  style={styles.touchable1}
-                  onPress={() => {
-                    setModalVisible(!modalVisible);
-                    console.log('save button pressed');
-                    console.log(
-                      '🚀 ~ file: WishlistScreen.js ~ line 70 ~ WishlistScreen ~ textInput2Value',
-                      textInput2Value,
-                    );
-                    console.log(
-                      '🚀 ~ file: WishlistScreen.js ~ line 71 ~ WishlistScreen ~ textInput3Value',
-                      textInput3Value,
-                    );
-                    const newValue = {title: textInput2Value, quantity: textInput3Value};
-                    setwishlistArr([...wishlistArr, newValue]);
-                    console.log(
-                      '🚀 ~ file: WishlistScreen.js ~ line 71 ~ WishlistScreen ~ wishlistArr',
-                      wishlistArr,
-                    );
-                  }}>
-                  <Text style={styles.text1}>Save</Text>
-                </TouchableHighlight>
-              </View>
+                let itemToAdd = {
+                  id: wishListArrayLength,
+                  title: itemName,
+                  quantity: itemQuantity,
+                };
+
+                setItemName("");
+                setItemQuantity("");
+                setWishlistArr([...wishlistArr, itemToAdd]);
+                setModalVisible(false);
+              }}>
+                  Add item
+              </Button>
             </View>
           </View>
         </View>
       </Modal>
-      <View style={styles.view2}>
+      <View style={{ flexGrow: 1}}>
         <FlatList
+          ListEmptyComponent={<View style={{ alignItems: 'center' }}><Subheading>Your Wishlist is empty</Subheading></View>}
           data={wishlistArr}
           renderItem={renderItem}
           keyExtractor={(item, index) => {
-            console.log('printing item ', item, ' and ', index);
             return `${index}`;
           }}
         />
       </View>
       <ActionButton
-        buttonColor="rgba(231,76,60,1)"
+        buttonColor={Colors.themeLightColors.primary}
         title="Add"
         useNativeFeedback={true}
         onPress={() => setModalVisible(true)}
@@ -127,141 +139,7 @@ const WishlistScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#efefef',
-    borderWidth: 1,
-  },
-  view1: {
-    flexShrink: 1,
-    borderWidth: 1,
-    borderColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view2: {
-    flexGrow: 1,
-    // borderWidth: 1,
-    // borderColor: 'red',
-    paddingHorizontal: 20,
-  },
-  view3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-    // borderColor: 'orange',
-    // borderWidth: 1,
-  },
-  view4: {
-    flexShrink: 1,
-    margin: 20,
-    width: '90%',
-    backgroundColor: 'white',
-    borderRadius: 20,
-    padding: 5,
-    alignItems: 'center',
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    // borderColor: 'orange',
-    borderWidth: 1,
-  },
-  view5: {
-    flexShrink: 1,
-    paddingVertical: 5,
-    paddingBottom: 15,
-    width: '90%',
-    // borderColor: 'blue',
-    // borderWidth: 1,
 
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view6: {
-    flexShrink: 1,
-    paddingVertical: 5,
-    width: '90%',
-    // borderColor: 'red',
-    // borderWidth: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view7: {
-    flexShrink: 1,
-
-    paddingVertical: 5,
-    width: '90%',
-    // borderColor: 'red',
-    // borderWidth: 1,
-
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view8: {
-    flexShrink: 1,
-    paddingVertical: 5,
-    paddingBottom: 10,
-    paddingTop: 15,
-    width: '90%',
-    // borderColor: 'red',
-    // borderWidth: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  view9: {
-    flexShrink: 1,
-    // borderWidth: 1,
-    width: '100%',
-    margin: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 20,
-  },
-  text1: {
-    fontSize: 16,
-    color: '#595959',
-  },
-  actionButtonIcon: {
-    fontSize: 20,
-    height: 22,
-  },
-  touchable1: {
-    height: 30,
-    width: '100%',
-    padding: 2,
-    backgroundColor: 'lightblue',
-    borderRadius: 5,
-    elevation: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    fontSize: 16,
-  },
-  textInput2: {
-    width: '100%',
-    height: 30,
-    borderWidth: 1,
-    borderRadius: 5,
-    fontSize: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
-  textInput3: {
-    width: '100%',
-    height: 30,
-    borderWidth: 1,
-    borderRadius: 5,
-    fontSize: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
-  },
 });
 
 export default WishlistScreen;
